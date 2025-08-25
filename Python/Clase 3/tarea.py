ef11be15a1db617fcee13ab906bd4e279564c856
Fernando Adaro <feermza@gmail.com>
2025-08-25 20:44:21 -0300

    Update tarea.py

--- Python/Clase 3/tarea.py
+++ Python/Clase 3/tarea.py
@@ -5,7 +5,7 @@ seleccionArgentina = {
     10: {'Nombre': 'Lionel Messi', 'Edad': 35, 'Altura': 1.70, 'Precio': '50 Millones', 'Posicion': 'Delantero'},
     11: {'Nombre': 'Ángel Di María', 'Edad': 34, 'Altura': 1.80, 'Precio': '12 Millones', 'Posicion': 'Mediocampista'},
     24: {'Nombre': 'Paulo Dybala', 'Edad': 28, 'Altura': 1.77, 'Precio': '35 Millones', 'Posicion': 'Mediocampista'},
-    19: {'Nombre': 'Nicolás Otamendi', 'Edad': 34, 'Altura': 1.83, 'Precio': '3.5 Millones', 'Posicion': 'Defensa'},
+    19: {'Nombre': 'Nicolás Otamendi', 'Edad': 34, 'Altura': 1.83, 'Precio': '3.5 Millones', 'Posicion': 'Defensa'}
 }
 
 # Agrego los nuevos jugadores
@@ -15,6 +15,7 @@ seleccionArgentina[7] = {'Nombre': 'Ángel Correa', 'Edad': 26, 'Altura': 1.70, 
 seleccionArgentina[3] = {'Nombre': 'Marcos Acuña', 'Edad': 30, 'Altura': 1.75, 'Precio': '8 Millones', 'Posicion': 'Defensa'}
 seleccionArgentina[9] = {'Nombre': 'Julian Alvarez', 'Edad': 25, 'Altura': 1.70, 'Precio':'90 millones de euros', 'Posicion': 'Delantero Extremo'}
 seleccionArgentina[3] = {'Nombre': 'Nicolas Tagliafico', 'Edad': 32, 'Altura': 1.70, 'Precio': '6 millones de euros', 'Posicion': 'Lateral Izquierdo'}
+seleccionArgentina[17] = {'Nombre' : 'Cristian Romero', 'Edad' : 27, 'Altura' : 185, 'Precio' : '10 Millones', 'Posicion' : 'Defensor Central'}
 
 # Muestro el el diccionario completo
 #print(seleccionArgentina)
