8357006559a8c6cb2f08f576997be614ac912dcd
Tomas Andres <tomas.andres520@gmail.com>
2025-08-25 20:17:02 -0300

    Tarea clase3 Diccionario y agrego 4 jugadores a este

---
+++ Python/Clase 3/tarea.py
@@ -0,0 +1,18 @@
+
+# Ejercicio con diccionario TAREA
+# Arranco mi diccionario 
+seleccionArgentina = {
+    10: {'Nombre': 'Lionel Messi', 'Edad': 35, 'Altura': 1.70, 'Precio': '50 Millones', 'Posicion': 'Delantero'},
+    11: {'Nombre': 'Ángel Di María', 'Edad': 34, 'Altura': 1.80, 'Precio': '12 Millones', 'Posicion': 'Mediocampista'},
+    24: {'Nombre': 'Paulo Dybala', 'Edad': 28, 'Altura': 1.77, 'Precio': '35 Millones', 'Posicion': 'Mediocampista'},
+    19: {'Nombre': 'Nicolás Otamendi', 'Edad': 34, 'Altura': 1.83, 'Precio': '3.5 Millones', 'Posicion': 'Defensa'},
+}
+
+# Agrego los nuevos jugadores
+seleccionArgentina[1] = {'Nombre': 'Franco Armani', 'Edad': 35, 'Altura': 1.89, 'Precio': '5 Millones', 'Posicion': 'Arquero'}
+seleccionArgentina[5] = {'Nombre': 'Leandro Paredes', 'Edad': 28, 'Altura': 1.80, 'Precio': '15 Millones', 'Posicion': 'Mediocampista'}
+seleccionArgentina[7] = {'Nombre': 'Ángel Correa', 'Edad': 26, 'Altura': 1.70, 'Precio': '18 Millones', 'Posicion': 'Delantero'}
+seleccionArgentina[3] = {'Nombre': 'Marcos Acuña', 'Edad': 30, 'Altura': 1.75, 'Precio': '8 Millones', 'Posicion': 'Defensa'}
+
+# Muestro el el diccionario completo
+print(seleccionArgentina)
