429a676c02ccf63d37a088eda47efe691bc8728b
Martín Raul Yañez <ytincho@gmail.com>
2025-08-27 20:21:58 -0300

    Update ejercicio3.parte2.java

---
+++ JAVA/CLase 3/ejercicio3.parte2.java
@@ -0,0 +1,24 @@
+package Ciclos03;
+import javax.swing.JOptionPane;
+
+public class Ciclos03 {
+    public static void main(String[] args) {
+        int numero;
+
+        do {
+            numero = Integer.parseInt(
+                JOptionPane.showInputDialog(null, "Digite un número (0 para salir):")
+            );
+
+            if (numero != 0) {
+                if (numero % 2 == 0) {
+                    JOptionPane.showMessageDialog(null, numero + " es PAR");
+                } else {
+                    JOptionPane.showMessageDialog(null, numero + " es IMPAR");
+                }
+            }
+        } while (numero != 0);
+
+        JOptionPane.showMessageDialog(null, "Programa finalizado.");
+    }
+}
